b70e9bf63eac9275b5b23c1b1d7d49e4f0e4ba76
Nikola Buljcik <nikola.buljcik@itengine.rs>
2023-10-23 13:43:23 +0200

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -36,7 +36,11 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     const onSubmit = handleSubmit(async (data) => {
         try {
             setSubmitting(true);
-            await axios.post('/api/issues', data);
+            if (issue) {
+                await axios.patch(`/api/issues/${issue.id}`, data);
+            } else {
+                await axios.post('/api/issues', data);
+            }
             router.push('/issues');
         } catch (error) {
             setSubmitting(false);
@@ -63,7 +67,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
                     render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
                 />
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
-                <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
+                <Button disabled={isSubmitting}>{issue ? 'Update Issue' : 'Submit New Issue'}{' '}{isSubmitting &&
+                    <Spinner />}</Button>
             </form>
         </div>
     );
