b41cf7088b581dc0161227b4a2dca59f368544c6
Nikola Buljcik <nikola.buljcik@itengine.rs>
2023-10-23 15:46:18 +0200

    Add a delete button

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -4,18 +4,18 @@ import ReactMarkdown from 'react-markdown';
 import { Issue } from '@prisma/client';
 
 const IssueDetails = ({ issue }: { issue: Issue }) => {
-  return (
-    <>
-      <Heading>{issue.title}</Heading>
-      <Flex className="space-x-3" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </>
-  );
+    return (
+        <>
+            <Heading>{issue.title}</Heading>
+            <Flex className="space-x-3" my="2">
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card className="prose max-w-full" mt="4">
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
+            </Card>
+        </>
+    );
 };
 
 export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,34 +1,38 @@
 import { notFound } from 'next/navigation';
-import { Box, Grid } from '@radix-ui/themes';
+import { Box, Flex, Grid } from '@radix-ui/themes';
 import prisma from '@/prisma/client';
 import EditIssueButton from '@/app/issues/[id]/EditIssueButton';
 import IssueDetails from '@/app/issues/[id]/IssueDetails';
+import DeleteIssueButton from '@/app/issues/[id]/DeleteIssueButton';
 
 interface Props {
-  params: { id: string };
+    params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  const issue = await prisma.issue.findUnique({
-    where: {
-      id: parseInt(params.id)
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: parseInt(params.id)
+        }
+    });
+
+    if (!issue) {
+        notFound();
     }
-  });
 
-  if (!issue) {
-    notFound();
-  }
-
-  return (
-    <Grid columns={{ initial: '1', md: '2' }} gap="5">
-      <Box>
-        <IssueDetails issue={issue} />
-      </Box>
-      <Box>
-        <EditIssueButton issueId={issue.id} />
-      </Box>
-    </Grid>
-  );
+    return (
+        <Grid columns={{ initial: '1', sm: '5' }} gap="5">
+            <Box className="md:col-span-4">
+                <IssueDetails issue={issue} />
+            </Box>
+            <Box>
+                <Flex direction="column" gap="4">
+                    <EditIssueButton issueId={issue.id} />
+                    <DeleteIssueButton issueId={issue.id} />
+                </Flex>
+            </Box>
+        </Grid>
+    );
 };
 
 export default IssueDetailPage;
