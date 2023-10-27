6b501699157b0d6c1edc506b5418dc856d6b870e
Nikola Buljcik <nikola.buljcik@itengine.rs>
2023-10-27 13:34:25 +0200

    Secure the application

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,21 +1,29 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/prisma/client';
 import { issueSchema } from '../../validationSchemas';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/api/auth/authOptions';
 
 export async function POST(request: NextRequest) {
-    const body = await request.json();
-    const validation = issueSchema.safeParse(body);
+  const session = await getServerSession(authOptions);
 
-    if (!validation.success) {
-        return NextResponse.json(validation.error.format(), { status: 400 });
+  if (!session) {
+    return NextResponse.json({}, { status: 401 });
+  }
+
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description
     }
+  });
 
-    const newIssue = await prisma.issue.create({
-        data: {
-            title: body.title,
-            description: body.description
-        }
-    });
-
-    return NextResponse.json(newIssue, { status: 201 });
+  return NextResponse.json(newIssue, { status: 201 });
 }
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,35 +4,39 @@ import prisma from '@/prisma/client';
 import EditIssueButton from '@/app/issues/[id]/EditIssueButton';
 import IssueDetails from '@/app/issues/[id]/IssueDetails';
 import DeleteIssueButton from '@/app/issues/[id]/DeleteIssueButton';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/api/auth/authOptions';
 
 interface Props {
-    params: { id: string };
+  params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-    const issue = await prisma.issue.findUnique({
-        where: {
-            id: parseInt(params.id)
-        }
-    });
+  const session = await getServerSession(authOptions);
 
-    if (!issue) {
-        notFound();
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id)
     }
+  });
 
-    return (
-        <Grid columns={{ initial: '1', sm: '5' }} gap="5">
-            <Box className="md:col-span-4">
-                <IssueDetails issue={issue} />
-            </Box>
-            <Box>
-                <Flex direction="column" gap="4">
-                    <EditIssueButton issueId={issue.id} />
-                    <DeleteIssueButton issueId={issue.id} />
-                </Flex>
-            </Box>
-        </Grid>
-    );
+  if (!issue) {
+    notFound();
+  }
+
+  return (
+    <Grid columns={{ initial: '1', sm: '5' }} gap="5">
+      <Box className="md:col-span-4">
+        <IssueDetails issue={issue} />
+      </Box>
+      {session && <Box>
+        <Flex direction="column" gap="4">
+          <EditIssueButton issueId={issue.id} />
+          <DeleteIssueButton issueId={issue.id} />
+        </Flex>
+      </Box>}
+    </Grid>
+  );
 };
 
 export default IssueDetailPage;
